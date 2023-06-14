e2116cb11d40a8fcf9debd9104f994a81aa4b590
LexZet <21840171+LexZet@users.noreply.github.com>
2023-06-14 21:03:19 +0300

    Update DockerServlet.java

--- src/main/java/org/cloudacademy/example/webapp/DockerServlet.java
+++ src/main/java/org/cloudacademy/example/webapp/DockerServlet.java
@@ -102,7 +102,7 @@ public class DockerServlet extends HttpServlet {
             response.getWriter().println(containerIp);
             break;
         case "/action3":
-            logger.debug("action 3 called...");
+            logger.debug("action 3 called... me");
             response.getWriter().println("action->3");
             response.getWriter().println(containerId);
             response.getWriter().println(containerIp);
